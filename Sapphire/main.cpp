0b5a0676533c44ebb0fa38342ccce87db7ec116b
Lord Vader <Owner-Apple@Owners-MacBook-Air.local>
2017-06-19 14:05:24 -0400

    Initial Commit

---
+++ Sapphire/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sapphire
+//
+//  Created by Owner on 6/19/17.
+//  Copyright © 2017 Owner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
